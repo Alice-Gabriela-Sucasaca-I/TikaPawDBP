485fd520e852e69bafcbac8a5de4679a7d76dea4
alicegabriela <sucasacaalice@gmail.com>
2025-05-13 15:49:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const usuariosRoutes = require('./routes/usuarios');//n
 const refugiosRoutes = require('./routes/refugios');//n
 
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
