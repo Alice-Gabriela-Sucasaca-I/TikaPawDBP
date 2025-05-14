b63c75d858b1b885d94f96718121a50f56cf50a2
alicegabriela <sucasacaalice@gmail.com>
2025-05-14 04:23:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express'); // servidor
 const bodyParser = require('body-parser'); // datos
 //const mysql = require('mysql2'); 
@@ -24,4 +25,42 @@ app.use('/refugios', refugiosRoutes); //n
 // iniciar servidor
 app.listen(port, () => {
     console.log('servidor corriendo en http://localhost:' + port);
+});
+*/
+const express = require('express');
+const session = require('express-session'); 
+const path = require('path');
+
+// Rutas
+const indexRoutes = require('./routes/index');
+const usuariosRoutes = require('./routes/usuarios');
+const refugiosRoutes = require('./routes/refugios');
+
+const app = express();
+const port = 3000;
+
+// Middlewares
+app.use(express.json());
+
+// Configuración seesion
+app.use(session({
+    secret: 'mi-secreto',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: process.env.NODE_ENV === 'production', 
+        httpOnly: true 
+    }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.use('/', indexRoutes);
+app.use('/usuarios', usuariosRoutes);
+app.use('/refugios', refugiosRoutes);
+
+// Iniciar servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
--- config/database.js
+++ config/database.js
@@ -28,6 +28,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'aliceg',
     database: process.env.DB_DATABASE || 'dbtikapaw',
+    //port: process.env.DB_PORT || 3306
     port: process.env.DB_PORT || 32803
 });
 
